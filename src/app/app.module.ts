598e5ade65e43f91da8ea687de2133c479c6a2f1
George Puisha <e.puysha@itransition.com>
2017-11-16 15:44:15 +0300

    Add auth service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
-import 'rxjs/Rx';
+import "rxjs/Rx";
 
 
 import { AppComponent } from "./app.component";
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import 'rxjs/add/operator/filter';
+import * as auth0 from 'auth0-js';
+
+@Injectable()
+export class AuthService {
+
+  constructor() { }
+
+}
