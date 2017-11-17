d69453e93cde0055b8ed345d0055a83e9579af6c
George Puisha <e.puysha@itransition.com>
2017-11-17 13:12:28 +0300

    Add login function

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,8 +1,24 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import "rxjs/add/operator/filter";
+import * as auth0 from "auth0-js";
 
 @Injectable()
 export class AuthService {
 
-  constructor() { }
+  auth0 = new auth0.WebAuth({
+    clientID: "BAEUD0UBR83uSbxpNJhHuLuGRjg2KEC5",
+    domain: "store-grabber.eu.auth0.com",
+    responseType: "token id_token",
+    audience: "https://store-grabber.eu.auth0.com/userinfo",
+    redirectUri: "http://localhost:4200/callback",
+    scope: "openid"
+  });
+
+  constructor(public router: Router) { }
+
+  public login(): void {
+    this.auth0.authorize();
+  }
 
 }
