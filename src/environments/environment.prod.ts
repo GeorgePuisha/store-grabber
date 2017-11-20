956457a26a03c0649597a7b5da69d581b67e7a65
George Puisha <stearis@tut.by>
2017-11-20 10:34:20 +0300

    Fix prod login issue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  URL: "store-grabber.herokuapp.com/",
-  API_URL: "store-grabber-api.herokuapp.com/api/",
+  URL: "https://store-grabber.herokuapp.com/",
+  API_URL: "https://store-grabber-api.herokuapp.com/api/",
   CLIENT_ID: "BAEUD0UBR83uSbxpNJhHuLuGRjg2KEC5"
 };
