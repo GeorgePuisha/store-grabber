bbe9f84ac70bcf7821375feb92f433987b111498
George Puisha <e.puysha@itransition.com>
2017-11-22 15:48:23 +0300

    Add search request

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,8 @@
+import { environment } from "../../../environments/environment";
+
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-search',
@@ -8,9 +12,26 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  searchTerm: string;
+
+  constructor(public http: HttpClient) { }
 
   ngOnInit() {
+  }
+
+  private onKey(event: any) {
+    this.searchTerm = event.target.value;
+    if (event.keyCode == 13) {
+      this.search();
+    }
+  }
+
+  private search() {
+    this.http
+      .get(environment.API_URL + "search/" + this.searchTerm)
+      .subscribe((data) => {
+
+      });
   }
 
 }
