2cbff0e852362c86de052523365eeec219485955
George Puisha <e.puysha@itransition.com>
2017-11-27 15:32:01 +0300

    Add watch button

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,11 @@
+import { environment } from "../../../environments/environment";
+
 import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
 import { Product } from "../product";
+
+import { HttpClient } from "@angular/common/http";
+
+import { AuthService } from "../../services/auth/auth.service";
 
 @Component({
   selector: 'app-product',
@@ -9,11 +15,29 @@ import { Product } from "../product";
 })
 export class ProductComponent implements OnInit {
 
+  profile: any;
   @Input() showedProducts: Product[];
 
-  constructor() { }
+  constructor(public http: HttpClient, public auth: AuthService) { }
 
   ngOnInit() {
+    if (this.auth.userProfile) {
+      this.profile = this.auth.userProfile;
+    } else {
+      this.auth.getProfile((err, profile) => {
+        this.profile = profile;
+      });
+    }
+  }
+
+  public watch(product: Product) {
+    console.log(product);
+    this.http
+      .get(environment.API_URL + "watch/" + this.profile.name + "/" + product.key)
+      .map((data) => JSON.stringify(data))
+      .subscribe((data) => {
+
+      });
   }
 
 }
